556029cdc5bf8cb54e5339261ead6a873a2e4c5a
he-nicolas <nicolas.herrmann@outlook.de>
2023-12-27 09:55:01 +0100

    Evaluate model on slices

--- starter/starter/ml/model.py
+++ starter/starter/ml/model.py
@@ -1,6 +1,7 @@
 from sklearn.metrics import fbeta_score, precision_score, recall_score
 from sklearn.ensemble import RandomForestClassifier
 
+
 # Optional: implement hyperparameter tuning.
 def train_model(X_train, y_train):
     """
--- starter/starter/train_model.py
+++ starter/starter/train_model.py
@@ -5,7 +5,7 @@ from sklearn.model_selection import train_test_split
 import pandas as pd
 import pickle
 from ml.data import process_data
-from ml.model import train_model
+from ml.model import train_model, inference, compute_model_metrics
 
 # Add code to load in the data.
 data = pd.read_csv("starter/data/census.csv")
@@ -30,10 +30,56 @@ X_train, y_train, encoder, lb = process_data(
 pickle.dump(encoder, open("starter/model/encoder.pickle", "wb"))
 
 # Proces the test data with the process_data function.
-X_test, y_test, encoder_test, lb_test = process_data(
-    test, categorical_features=cat_features, label="salary", training=True
+X_test, y_test, _, _ = process_data(
+    test,
+    categorical_features=cat_features,
+    label="salary",
+    training=False,
+    encoder=encoder,
+    lb=lb,
 )
 
 # Train and save a model.
 model = train_model(X_train, y_train)
 pickle.dump(model, open("starter/model/model.pickle", "wb"))
+
+
+def compute_model_metrics_on_slices(
+    model,
+    data,
+    cat_features,
+    encoder,
+    lb,
+    fixed_feature,
+    fixed_value,
+    print_result=False,
+):
+    relevant_data = data[data[fixed_feature] == value]
+    relevant_X_test, relevant_y_test, _, _ = process_data(
+        relevant_data,
+        categorical_features=cat_features,
+        label="salary",
+        training=False,
+        encoder=encoder,
+        lb=lb,
+    )
+    relevant_preds = inference(model, relevant_X_test)
+
+    precision, recall, fbeta = compute_model_metrics(
+        relevant_y_test,
+        relevant_preds)
+
+    if print_result:
+        print("Performance for", fixed_feature, "==", fixed_value, ":")
+        print("Precision:", precision)
+        print("Recall:", recall)
+        print("Fbeta:", fbeta)
+
+    return precision, recall, fbeta
+
+
+# Print performance on model slices (education fixed)
+for value in test["education"].unique():
+    performance = compute_model_metrics_on_slices(
+        model, test, cat_features, encoder, lb, "education", value, True
+    )
