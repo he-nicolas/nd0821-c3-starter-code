61938618eaade2172488690fa3efbc7420efd3f6
he-nicolas <nicolas.herrmann@outlook.de>
2023-12-27 17:33:17 +0100

    Add images

---
+++ request.py
@@ -0,0 +1,25 @@
+import requests
+import json
+
+data = json.dumps({
+    "age": 39,
+    "workclass": "State-gov",
+    "fnlgt": 77516,
+    "education": "Bachelors",
+    "education-num": 13,
+    "marital-status": "Never-married",
+    "occupation": "Adm-clerical",
+    "relationship": "Not-in-family",
+    "race": "White",
+    "sex": "Male",
+    "capital-gain": 2174,
+    "capital-loss": 0,
+    "hours-per-week": 40,
+    "native-country": "United-States"
+})
+
+url = "https://mlops-service.onrender.com/inference"
+response = requests.post(url, data=data)
+
+print(response.status_code)
+print(response.json())
